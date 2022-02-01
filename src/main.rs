9500ad3a255f9a6a678a21bbee0a92f195c07193
Tom Haines <tom@tomhaines.xyz>
2022-02-01 14:52:21 +0000

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+//! Blinks the LED on a Pico board
+//!
+//! This will blink an LED attached to GP25, which is the pin the Pico uses for the on-board LED.
+#![no_std]
+#![no_main]
+
+use cortex_m_rt::entry;
+use defmt::*;
+use defmt_rtt as _;
+use panic_probe as _;
+
+use cortex_m::asm;
+
+// Provide an alias for our BSP so we can switch targets quickly.
+// Uncomment the BSP you included in Cargo.toml, the rest of the code does not need to change.
+use rp2040_hal as _;
+// use sparkfun_pro_micro_rp2040 as bsp;
+
+#[link_section = ".boot_loader"]
+#[used]
+pub static BOOT_LOADER: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
+
+#[entry]
+fn main() -> ! {
+    info!("Program start");
+
+    loop {
+        asm::nop();
+    }
+}
+
+// End of file
